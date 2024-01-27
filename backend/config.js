9f7bfae73fbfa3172916e602a76b052f9ca0db30
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-27 10:17:21 +0530

    Connect to MongoDb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const mongoDBURL =
+  'mongodb+srv://Ray:Rayofhope@bookstoredb.zt5g6cw.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import { PORT } from './config.js';
+import { mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is Doing Dota to port : ${PORT}`);
+app.get('/', (req, res) => {
+  return res.status(200).send('<h1>Get Request To Home Sucess</h1>');
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to Databse');
+    app.listen(PORT, () => {
+      console.log(`App is Doing Dota to port : ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
