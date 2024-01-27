88d38cdb005e5f3c3758fd6f25872f6e3a8dd1b6
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-27 16:18:52 +0530

    Setup CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express, { request, response } from 'express';
 import { PORT } from './config.js';
 import { mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-
+import cors from 'cors';
 import booksRoute from './routes/booksRoute.js';
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for CORS
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (req, res) => {
   return res.status(200).send('<h1>Get Request To Home Sucess</h1>');
