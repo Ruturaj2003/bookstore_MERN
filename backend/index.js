f97bed5b92d348d30723a1ee54924341cc15a055
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-27 10:47:47 +0530

    Add Data via POST

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,36 @@ import express from 'express';
 import { PORT } from './config.js';
 import { mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.status(200).send('<h1>Get Request To Home Sucess</h1>');
+});
+
+app.post('/books', async (req, resp) => {
+  const { body } = req;
+  try {
+    if (!body.title || !body.author || !body.publishYear) {
+      return resp.status(400).send({
+        message: 'Send All The required Fields',
+      });
+    }
+
+    const newBook = {
+      title: body.title,
+      author: body.author,
+      publishYear: body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return resp.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    resp.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
